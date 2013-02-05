39646778dd7eb687f4246432e265e15e229289f3
Greg Mercer <gmercer@stanford.edu>
2013-02-05 08:45:17 -0800

    Cleaned up a little bit of the code as a follow-up to last commit.

--- gsb_feature_slideshow.js
+++ gsb_feature_slideshow.js
@@ -8,19 +8,7 @@ Drupal.settings.gsb_slideshow_settings = Drupal.settings.gsb_slideshow_settings 
    attach: function (context, settings) {
      if ($('.field-name-field-gsb-slideshow').length) {
        var rotation_time = Drupal.settings.gsb_slideshow_settings.rotation_time;
-       $('.field-name-field-gsb-slideshow').tabs({fx:{width: "toggle"}}).tabs("rotate", rotation_time, true);
-       //$('.field-name-field-gsb-slideshow').tabs({ fx: { height: 'toggle' } });
-       //$('.field-name-field-gsb-slideshow').tabs({fx:{width: "toggle"}}).tabs("rotate", rotation_time, true);
-       /*
-      http://jquerytools.org/documentation/tabs/slideshow.html
-       $('.field-name-field-gsb-slideshow').tabs(".images > div", {
-            // enable "cross-fading" effect
-            effect: 'fade',
-            fadeOutSpeed: "slow",
-            // start from the beginning after the last tab
-            rotate: true
-            // use the slideshow plugin. It accepts its own configuration
-            }).slideshow(); */
+       $('.field-name-field-gsb-slideshow').tabs().tabs("rotate", rotation_time, true);
      }
    }
  }
